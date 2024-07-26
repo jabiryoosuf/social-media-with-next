1ece42ccf644ad5de255eacdfd79a2c2e6648395
jabiryoosuf <jabiribnuyoosuf@gmail.com>
2024-07-27 00:19:09 +0530

    added navbar and stories section

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,5 @@
+const ProfilePage = () => {
+  return <div>profilwe</div>;
+};
+
+export default ProfilePage;
---
+++ src/components/Stories.tsx
@@ -0,0 +1,93 @@
+import Image from "next/image";
+
+const Stories = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-xs overflow-scroll scrolbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* stories */}
+        <div className=" flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20877343/pexels-photo-20877343/free-photo-of-smiling-mother-and-son-hugging.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 ring-2 rounded-full"
+          />
+          <span className="font-medium">wicky</span>
+        </div>
+        <div className=" flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20877343/pexels-photo-20877343/free-photo-of-smiling-mother-and-son-hugging.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 ring-2 rounded-full"
+          />
+          <span className="font-medium">wicky</span>
+        </div>
+        <div className=" flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20877343/pexels-photo-20877343/free-photo-of-smiling-mother-and-son-hugging.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 ring-2 rounded-full"
+          />
+          <span className="font-medium">wicky</span>
+        </div>
+        <div className=" flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20877343/pexels-photo-20877343/free-photo-of-smiling-mother-and-son-hugging.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 ring-2 rounded-full"
+          />
+          <span className="font-medium">wicky</span>
+        </div>
+        <div className=" flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20877343/pexels-photo-20877343/free-photo-of-smiling-mother-and-son-hugging.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 ring-2 rounded-full"
+          />
+          <span className="font-medium">wicky</span>
+        </div>
+        <div className=" flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20877343/pexels-photo-20877343/free-photo-of-smiling-mother-and-son-hugging.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 ring-2 rounded-full"
+          />
+          <span className="font-medium">wicky</span>
+        </div>
+        <div className=" flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20877343/pexels-photo-20877343/free-photo-of-smiling-mother-and-son-hugging.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 ring-2 rounded-full"
+          />
+          <span className="font-medium">wicky</span>
+        </div>
+        <div className=" flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20877343/pexels-photo-20877343/free-photo-of-smiling-mother-and-son-hugging.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 ring-2 rounded-full"
+          />
+          <span className="font-medium">wicky</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stories;

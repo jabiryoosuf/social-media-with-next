b46e1ab7787e42515ad8d07fdad5c6979394d9f7
jabiryoosuf <jabiribnuyoosuf@gmail.com>
2024-08-08 23:59:04 +0530

    complete ads

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,5 +1,51 @@
+import { Six_Caps } from "next/font/google";
+import Image from "next/image";
+
 const Ad = ({ size }: { size: "sm" | "md" | "lg" }) => {
-  return <div className="p-4 bg-white rounded-lg shadow-md text-sm">ad</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm">
+      {/* top  */}
+      <div className="flex items-center justify-between text-gray-500 font-medium">
+        <span>Sponsored Ads</span>
+        <Image src="/more.png" alt="" width={16} height={16} />
+      </div>
+      {/* bottom  */}
+      <div
+        className={` flex flex-col mt-4 ${size === "sm" ? "gap-2" : "gap-4"}`}
+      >
+        <div
+          className={`relative w-full ${
+            size === "sm" ? "h-24" : size == "md" ? "h-36" : "h-48"
+          }`}
+        >
+          <Image
+            src="https://images.pexels.com/photos/27275513/pexels-photo-27275513/free-photo-of-cappadocia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/27275513/pexels-photo-27275513/free-photo-of-cappadocia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={24}
+            height={24}
+            className="rounded-full w-6 h-6 object-cover"
+          />
+          <span className="text-blue-500">Big chef Lounge</span>
+        </div>
+        <p className={`size=== "sm` ? "text-xs" : "text-sm"}>
+          {size === "sm"
+            ? "We’ve trained a model called ChatGPT which interacts in a conversational way. The dialogue format makes it possible for ChatGPT to answer followup questions"
+            : size === "md"
+            ? "We’ve trained a model called ChatGPT which interacts in a conversational way. The dialogue format makes it possible for ChatGPT to answer followup questions, admit its mistakes, challenge incorrect premises, and reject inappropriate requests."
+            : "It’s difficult to say without more information about what the code is supposed to do and what’s happening when it’s executed. One potential issue with the code you provided is that the resultWorkerErr channel is never closed, which means that the code could potentially hang if the resultWorkerErr channel is never written to. This could happen if b.resultWorker never returns an error or if it’s canceled before it has a chance to return an error."}
+        </p>
+        <button className= " bg-gray-200 p-2 text-sm rounded-lg text-gray-500">Lern more</button>
+      </div>
+    </div>
+  );
 };
 
 export default Ad;
